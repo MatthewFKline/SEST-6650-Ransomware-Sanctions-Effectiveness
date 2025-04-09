1e73ba9c70b95e80a56d7071fcdc988c6eb1f7a9
Matthew Kline <31708500+MatthewFKline@users.noreply.github.com>
2025-04-08 23:31:56 -0400

    Start querying blockchain[.]com

---
+++ blockchain_com.py
@@ -0,0 +1,25 @@
+import json
+import time
+
+import requests
+
+# results = requests.get("https://blockchain.info/rawaddr/1Hhe61Bwxs8Hd2WxzWY9FQyZicBiZGeSNW")
+
+BASE_URL = "https://blockchain.info/rawaddr/"
+
+# content = results.content
+# text = results.text
+# data = results.json()
+
+with open("data/ransomware_data.json", "r") as f:
+    ransom_addresses = json.load(f)
+
+ransom_addresses = [item["address"] for item in ransom_addresses]
+
+for item in ransom_addresses:
+    response = requests.get(BASE_URL + item)
+    response.raise_for_status()
+    data = response.json()
+    if data["n_tx"] == 0:
+        print(f"No transactions observed for {item}")
+    time.sleep(10.5)
